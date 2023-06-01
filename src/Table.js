90de9a87895994a794bb3dee5072723f8a828247
Alexandru Dobra-Somesan <alexdobras@gmail.com>
2023-06-01 23:28:09 +0300

    improvements on reusability

--- src/Table.js
+++ src/Table.js
@@ -14,67 +14,80 @@ export default function Table() {
   function customFilterAndSearch(
     value, // value of the column filter input / table search input, should come from the table's `customFilterAndSearch`
     rowData, // data of a table row, should come from table's `customFilterAndSearch`
-    columnName, // name of the table column field
+    columnField, // name of the table column field
     filtersArray, // all the filters saved from table in a react state -> `useState([])`
     searchTerm, // value of the table search input saved in a react state -> `useState("")`
-    lookupKeys = [], // array of strings; the fields that should be searched for `searchTerm`, example: ["name", "surname", "birth"]
+    lookupColumnFields = [], // array of strings; all the (necessary) fields that should be included for searching based on `searchTerm`, example: ["name", "surname", "birth"]
     FILTER_CONDITION // the condition for Filter to return `true` as in accepted row for display
   ) {
-    console.log(value, rowData, columnName);
+    console.log(value, rowData, columnField);
 
-    if (lookupKeys.length === 0) lookupKeys = [...rowData];
+    if (lookupColumnFields.length === 0)
+      lookupColumnFields = [...Object.keys(rowData)];
 
     // CONDITION FOR FILTER SEARCH:
+
+    const termExistsOnRow = (field) => {
+      let searchTermIsFoundInRow = null;
+      const fieldValue = rowData[field];
+
+      if (typeof fieldValue === "string") {
+        const isTrue = fieldValue
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+        if (isTrue) {
+          return (searchTermIsFoundInRow = fieldValue);
+        }
+      }
+      if (typeof fieldValue === "number") {
+        const isTrue = fieldValue.toString().includes(searchTerm);
+        if (isTrue) {
+          return (searchTermIsFoundInRow = fieldValue);
+        }
+      }
+      if (typeof fieldValue === "object") {
+        const isTrue = Object.keys(fieldValue)
+          .map((field) => fieldValue[field].toLowerCase())
+          .join("")
+          .includes(searchTerm.toLowerCase());
+        if (isTrue) {
+          return (searchTermIsFoundInRow = fieldValue);
+        }
+      }
+      if (Array.isArray(fieldValue)) {
+        const isTrue = fieldValue
+          .map((field) => field.toLowerCase())
+          .includes(searchTerm.toLowerCase());
+        if (isTrue) {
+          return (searchTermIsFoundInRow = fieldValue);
+        }
+      }
+      return searchTermIsFoundInRow ? true : false;
+    };
+
     const columFilterValue = filtersArray.find(
-      (filter) => filter.column.field === columnName
+      (filter) => filter.column.field === columnField
     )?.value;
 
     if (columFilterValue) {
       // FLTER SEARCH
-      let searchTermIsFoundInRow = null;
 
-      lookupKeys.forEach((key) => {
-        if (typeof rowData[key] === "string") {
-          const isTrue = rowData[key].includes(searchTerm);
-          if (isTrue) {
-            return (searchTermIsFoundInRow = rowData[key]);
-          }
-        }
-        if (typeof rowData[key] === "number") {
-          const isTrue = rowData[key].toString().includes(searchTerm);
-          if (isTrue) {
-            return (searchTermIsFoundInRow = rowData[key]);
-          }
-        }
-        if (typeof rowData[key] === "object") {
-          const isTrue = Object.keys(rowData[key])
-            .map((key) => rowData[key][key])
-            .join("")
-            .includes(searchTerm);
-          if (isTrue) {
-            return (searchTermIsFoundInRow = rowData[key]);
-          }
-        }
-        if (Array.isArray(rowData[key])) {
-          const isTrue = rowData[key].includes(searchTerm);
-          if (isTrue) {
-            return (searchTermIsFoundInRow = rowData[key]);
-          }
-        }
-      });
+      const termExistsInAnyCell = lookupColumnFields.some(termExistsOnRow);
 
       // examples of FILTER_CONDITION:
-      // rowData[columnName].includes(value);
+      // rowData[columnField].includes(value);
       // or
-      // rowData[columnName].length > value.length;
+      // rowData[columnField].length > value.length;
 
       const conditionForFilteringisTrue =
-        searchTermIsFoundInRow && FILTER_CONDITION;
+        termExistsInAnyCell && FILTER_CONDITION;
 
       return conditionForFilteringisTrue ? true : false;
     } else {
       // TABLE SEARCH
-      const conditionForSearch = rowData[columnName].includes(searchTerm);
+      // const conditionForSearch = rowData[columnField].includes(searchTerm);
+      const conditionForSearch = termExistsOnRow(columnField);
+
       return conditionForSearch ? true : false;
     }
   }
@@ -109,20 +122,48 @@ export default function Table() {
                 this.field,
                 filtersArray,
                 searchTerm,
-                ["name", "surname", "birth"],
-                rowData[this.field].includes(value)
+                ["name", "surname", "age"],
+                rowData[this.field].toLowerCase().includes(value.toLowerCase())
               );
             },
           },
           {
             title: "Surname",
             field: "surname",
+            defaultFilter: filtersArray.find(
+              (filter) => filter.column.field === "surname"
+            )?.value,
+            customFilterAndSearch: function (value, rowData) {
+              return customFilterAndSearch(
+                value,
+                rowData,
+                this.field,
+                filtersArray,
+                searchTerm,
+                ["name", "surname", "age"],
+                rowData[this.field].toLowerCase().includes(value.toLowerCase())
+              );
+            },
           },
-          {
-            title: "Age",
-            field: "birth",
-            type: "numeric",
-          },
+          // {
+          //   title: "Age",
+          //   field: "age",
+          //   type: "numeric",
+          //   defaultFilter: filtersArray.find(
+          //     (filter) => filter.column.field === "age"
+          //   )?.value,
+          //   customFilterAndSearch: function (value, rowData) {
+          //     return customFilterAndSearch(
+          //       value,
+          //       rowData,
+          //       this.field,
+          //       filtersArray,
+          //       searchTerm,
+          //       ["name", "surname", "age"],
+          //       rowData[this.field].toString().includes(value)
+          //     );
+          //   },
+          // },
           //   {
           //     title: "City",
           //     field: "city",
@@ -133,19 +174,19 @@ export default function Table() {
           {
             name: "aa",
             surname: "Baran",
-            birth: 1987,
+            age: 1987,
             // city: 1,
           },
           {
             name: "ehm",
             surname: "abaa",
-            birth: 1980,
+            age: 1980,
             // city: 2,
           },
           {
             name: "aaab",
             surname: "bb",
-            birth: 1900,
+            age: 1900,
             // city: 1,
           },
         ]}

de19e10a5269e219c6c49ea9c83e9464a6f45b36
Alexandru Dobra-Somesan <alexdobras@gmail.com>
2023-06-12 05:02:42 +0300

    link library

--- src/Table.js
+++ src/Table.js
@@ -2,6 +2,7 @@ import MaterialTable from "material-table";
 import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
+import { customFilterAndSearch } from "material-table-custom-filter-and-search";
 
 export default function Table() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,85 +12,8 @@ export default function Table() {
     console.log("searchTerm", searchTerm);
   }, [searchTerm]);
 
-  function customFilterAndSearch(
-    value, // value of the column filter input / table search input, should come from the table's `customFilterAndSearch`
-    rowData, // data of a table row, should come from table's `customFilterAndSearch`
-    columnField, // name of the table column field
-    filtersArray, // all the filters saved from table in a react state -> `useState([])`
-    searchTerm, // value of the table search input saved in a react state -> `useState("")`
-    lookupColumnFields = [], // array of strings; all the (necessary) fields that should be included for searching based on `searchTerm`, example: ["name", "surname", "birth"]
-    FILTER_CONDITION // the condition for Filter to return `true` as in accepted row for display
-  ) {
-    console.log(value, rowData, columnField);
-
-    if (lookupColumnFields.length === 0)
-      lookupColumnFields = [...Object.keys(rowData)];
-
-    // CONDITION FOR FILTER SEARCH:
-
-    const termExistsOnRow = (field) => {
-      let searchTermIsFoundInRow = null;
-      const fieldValue = rowData[field];
-
-      if (typeof fieldValue === "string") {
-        const isTrue = fieldValue
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase());
-        if (isTrue) {
-          return (searchTermIsFoundInRow = fieldValue);
-        }
-      }
-      if (typeof fieldValue === "number") {
-        const isTrue = fieldValue.toString().includes(searchTerm);
-        if (isTrue) {
-          return (searchTermIsFoundInRow = fieldValue);
-        }
-      }
-      if (Array.isArray(fieldValue)) {
-        const isTrue = fieldValue
-          .map((field) => field.toLowerCase())
-          .includes(searchTerm.toLowerCase());
-        if (isTrue) {
-          return (searchTermIsFoundInRow = fieldValue);
-        }
-      }
-      if (typeof fieldValue === "object") {
-        const isTrue = Object.keys(fieldValue)
-          .map((field) => fieldValue[field].toLowerCase())
-          .join("")
-          .includes(searchTerm.toLowerCase());
-        if (isTrue) {
-          return (searchTermIsFoundInRow = fieldValue);
-        }
-      }
-      return searchTermIsFoundInRow ? true : false;
-    };
-
-    const columFilterValue = filtersArray.find(
-      (filter) => filter.column.field === columnField
-    )?.value;
-
-    if (columFilterValue) {
-      // FLTER SEARCH
-
-      const termExistsInAnyCell = lookupColumnFields.some(termExistsOnRow);
-
-      // examples of FILTER_CONDITION:
-      // rowData[columnField].includes(value);
-      // or
-      // rowData[columnField].length > value.length;
-
-      const conditionForFilteringisTrue =
-        termExistsInAnyCell && FILTER_CONDITION;
-
-      return conditionForFilteringisTrue ? true : false;
-    } else {
-      // TABLE SEARCH
-      // const conditionForSearch = rowData[columnField].includes(searchTerm);
-      const conditionForSearch = termExistsOnRow(columnField);
-
-      return conditionForSearch ? true : false;
-    }
+  function defaultFilter(field) {
+    return filtersArray?.find((filter) => filter.column.field === field)?.value;
   }
 
   return (
@@ -116,9 +40,7 @@ export default function Table() {
             //   (filter) => filter.column.field === "name"
             // )?.value,
             get defaultFilter() {
-              return filtersArray.find(
-                (filter) => filter.column.field === this.field
-              )?.value;
+              return defaultFilter(this.field);
             },
             customFilterAndSearch: function (value, rowData) {
               return customFilterAndSearch(
@@ -128,7 +50,7 @@ export default function Table() {
                 filtersArray,
                 searchTerm,
                 ["name", "surname", "age"],
-                rowData[this.field].toLowerCase().includes(value.toLowerCase())
+                rowData[this.field]?.toLowerCase().includes(value.toLowerCase())
               );
             },
           },
